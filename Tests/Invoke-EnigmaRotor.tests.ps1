b702cc465f2e91165d7a3ed52702b1dbf09ee4a8
Drewry Pope <drewrypope@gmail.com>
2018-03-12 14:26:02 -0500

    function returns valid map fails

--- Tests/Invoke-EnigmaRotor.tests.ps1
+++ Tests/Invoke-EnigmaRotor.tests.ps1
@@ -4,4 +4,10 @@ Describe 'Invoke-EnigmaRotor' {
     It 'Exists as a Function' {
         Test-Path Function:\Invoke-EnigmaRotor | Should -be $true
     }
+    It 'Returns an EnigmaRotor HashMap' {
+    	$x = Invoke-EnigmaRotor
+    	$x.keys.length | Should -be 27
+    	$x.position | Should -be 10
+    	$x.currentPosition | Should -be 0
+    }
 }
