6893691ce087e3b7f8191735a594132368a45b97
Drewry Pope <drewrypope@gmail.com>
2018-03-12 11:57:39 -0500

    Made test shouldBeCommutative. Accidentally passes.

--- PoshEnigmaMachine/Convert-EnigmaMessage.ps1
+++ PoshEnigmaMachine/Convert-EnigmaMessage.ps1
@@ -5,6 +5,7 @@ function Convert-EnigmaMessage {
         [String]$rotor2 = 'AJDKSIRUXBLHWTMCQGZNPYFVOE ',
         [String]$rotor3 = 'BDFHJLCPRTXVZNYEIWGAKMUSQO ',
         [String]$reflector = 'EJMZALYXVBWFCRQUONTSPIKHGD ',
+        [parameter(ValueFromPipeline)]
         [String]$message = 'AYYTC IMLGF UUEFW LAOGU TZOXE CRMIM JFMPY QRJDW DKBLO JPCFV UDDSF RPJFA RZOCU BXXMY TAJLC IOMQY ADZUS GYIOC MPFKM HRTFX N'
     )
     $message
--- Tests/Convert-EnigmaMessage.tests.ps1
+++ Tests/Convert-EnigmaMessage.tests.ps1
@@ -1,10 +1,14 @@
 ## Ensure the function is available
 Import-Module "$PSScriptRoot\..\PoshEnigmaMachine\PoshEnigmaMachine.psm1" -Force
 Describe 'Convert-EnigmaMessage' {
-    It 'Exists as a function' {
+    It 'Exists as a Function' {
         Test-Path Function:\Convert-EnigmaMessage | Should -be $true
     }
-    It 'Should return a [String]' {
+    It 'Returns a [String]' {
         Convert-EnigmaMessage | Should BeOfType [String]
     }
+    It 'Is Commutative' {
+    	'Hello' | Convert-EnigmaMessage | Convert-EnigmaMessage | Should -be 'Hello'
+    }
+
 }
